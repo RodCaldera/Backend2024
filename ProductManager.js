65cfd884651d205110f41d85019d2c7b0ad1a2ca
Rodrigo Caldera <rodrigo_caldera@hotmail.com>
2024-01-29 14:11:47 -0600

    First commit

---
+++ ProductManager.js
@@ -0,0 +1,50 @@
+class ProductManager{
+    static lastID=0
+    constructor(){
+         this.products=[];
+    
+    addProduct(title, description, price, thumbnail, code,stock); {
+        let newProduct={
+            title: title,
+            description: description,
+            price: price,
+            thumbnail: thumbnail,
+            code: code,
+            stock: stock,
+            id: ++ProductManager.lastID,
+        }
+        console.log (newProduct);
+        if (products.includes(this.code)){
+            console.log("Error! Code already exists. Please replace code.")
+            }
+         else{products.push(newProduct);}
+    }
+
+    getProducts();{
+        if (this.products.length===0){
+            console.log("Error: There are no products available.")
+        }
+        else{
+            console.log("List of Products:")
+            this.products.forEach(product=>{
+                console.log(`ID:${product.id}, Title:${product.title}, Description:${product.description}, Price:${product.price}, Thumbnail:${product.thumbnail}, Code:${product.code}, Stock:${product.stock}`)
+            })
+        }
+    }
+    getProductById(id);{
+        const product = this.products.find(product=> product.id===id)
+        if (product){
+            console.log(`Product Found - ID: ${product.id}, Title: ${product.title}, Price: $${product.price.toFixed(2)}, Stock: ${product.stock}`)
+        } 
+        else{
+            console.log(`Error: Product with ID ${id} not found`)
+        } 
+    }
+}
+}
+
+
+let newProduct = new ProductManager();
+
+newProduct.addProduct("Producto Prueba", "Este es un producto prueba", 200, "Sin imagen", "abc123", 25)
+
